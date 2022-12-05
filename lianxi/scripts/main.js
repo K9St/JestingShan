3c293818985b2a3f79cd595e97bf159df284736f
K9St <86698071+K9St@users.noreply.github.com>
2022-12-05 17:21:13 +0800

    Add files via upload
    1

---
+++ lianxi/scripts/main.js
@@ -0,0 +1,26 @@
+let myHeading = document.querySelector('h1');
+
+myHeading.onclick=function(){
+
+myHeading.textContent="wweeeeeeeeeeeeee";
+};
+
+
+let myButton = document.querySelector('button');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+   setUserName();
+}
